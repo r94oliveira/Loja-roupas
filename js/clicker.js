200abc1b1dbef9e95014a8db50d9d97e2fc36c1f
pietr <pietro.giacomitti@outlook.com>
2021-02-15 21:14:27 -0300

    pasta js adicionada ao repositório

---
+++ js/clicker.js
@@ -0,0 +1,9 @@
+document.getElementById("login").addEventListener("click", manut);
+
+function manut() {
+  for(var i=0; i<9; i++){
+   alert ("Estamos sob manutenção no momento.\n \
+   Por favor, tente novamente mais tarde.");
+   }
+   close();
+}
