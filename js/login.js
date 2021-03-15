e150d101b2528accea80909d7cba1b7c945dbf60
Robson Oliveira <r94oliveira@gmail.com>
2021-03-15 00:34:37 -0300

    altera php e js no login

---
+++ js/login.js
@@ -0,0 +1,35 @@
+
+        var btn = document.getElementById('form-submit');
+
+        btn.addEventListener('click', () => {
+
+		var emailjs = document.getElementById('email').value;
+		var senhajs = document.getElementById('senha').value;
+
+    if(emailjs == "" || emailjs == null){
+
+      $("#erro-email").html("Email é obrigatório.");
+
+      alert('Preencha todos os campos!')
+      
+      return(false);
+
+    }
+
+    if(senhajs == "" || senhajs == null){
+
+      $("#erro-senha").html("Senha é obrigatório.");
+
+      alert('Preencha todos os campos!')
+      
+      return(false);
+
+    }
+
+   	return(true);
+
+});
+
+ 
+
+
